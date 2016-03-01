990954f2f1d9e4a898aca4fcf5ea3c45426badf8
Michael Meier <mmeier61@yahoo.com>
2016-03-01 09:52:25 -0600

    home page with form

--- db/schema.rb
+++ db/schema.rb
@@ -15,4 +15,22 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "todos", force: :cascade do |t|
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "title"
+    t.text     "description"
+    t.boolean  "completed"
+    t.integer  "user_id"
+    t.integer  "contributor_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "email"
+    t.string   "password"
+  end
+
 end
