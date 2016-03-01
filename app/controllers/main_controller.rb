56cf6032baee4115995dfab9adfce7dd517d3615
Michael Meier <mmeier61@yahoo.com>
2016-03-01 10:05:04 -0600

    logout feature

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,10 +4,16 @@ end
 
 MyApp.post "/login_user" do
   @user = find_by_email(params["email"])
-  if @user != nil $$ @user.password == params["password"]
+  if @user != nil && @user.password == params["password"]
     session["user_id"] = @user.user_id
     redirect "welcome"
   else
     erb :"login_fail"
   end
+end
+
+MyApp.get "/logout" do
+  session["user_id"] = nil
+  redirect "login/home"
+  
 end
