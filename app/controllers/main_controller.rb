b1cae6333a382662f37690c482c3502de8c3c3eb
Michael Meier <mmeier61@yahoo.com>
2016-03-01 10:02:04 -0600

    controller to login

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,13 @@
 MyApp.get "/" do
   erb :"login/home"
 end
+
+MyApp.post "/login_user" do
+  @user = find_by_email(params["email"])
+  if @user != nil $$ @user.password == params["password"]
+    session["user_id"] = @user.user_id
+    redirect "welcome"
+  else
+    erb :"login_fail"
+  end
+end
