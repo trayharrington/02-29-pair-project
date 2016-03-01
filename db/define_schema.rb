b778a54330eff4f86c5eb1537df488ed355459b9
Michael Meier <mmeier61@yahoo.com>
2016-03-01 10:19:44 -0600

    Database designed

--- db/define_schema.rb
+++ db/define_schema.rb
@@ -1,5 +1,13 @@
 require_relative "./_configure"
+DB.define_table("users")
+DB.define_column("users","name","string")
+DB.define_column("users","email","string")
+DB.define_column("users","password","string")
 
+DB.define_table("errors")
+DB.define_column("errors","error_name","string")
+DB.define_column("errors","error_description","text")
+DB.define_column("errors","user_id","string")
 # DB.define_table("users")
 # DB.define_column("users", "name", "string")
 # DB.define_column("users", "email", "string")
